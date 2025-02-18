768a2839cf1d46d96403286c0c07e3aa22a80011
dustyhardman <40414792+dustyhardman@users.noreply.github.com>
2025-02-17 21:54:02 -0500

    new file:   main.py new file:   requirements.txt

---
+++ main.py
@@ -0,0 +1,11 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+
+
+def main():
+    print("Starting asteroids!")
+
+
+if __name__ == "__main__":
+    main()
