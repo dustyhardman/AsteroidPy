414f9f23432f21c42b3b4911d503146fdba38594
dustyhardman <40414792+dustyhardman@users.noreply.github.com>
2025-02-18 22:17:16 -0500

    new file:   .gitignore new file:   constants.py modified:   main.py

--- main.py
+++ main.py
@@ -3,9 +3,13 @@
 # throughout this file
 import pygame  # noqa: F401
 
+from constants import *
+
 
 def main():
     print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
