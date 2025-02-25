eb195fbb8ab2a3c0de7d18117add3dca3d81f724
dustyhardman <40414792+dustyhardman@users.noreply.github.com>
2025-02-24 20:19:14 -0500

    modified:   constants.py modified:   player.py

--- player.py
+++ player.py
@@ -20,13 +20,14 @@ class Player(CircleShape):
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
-        pygame.draw.circle(
-            screen, (255, 0, 0), (int(self.position.x), int(self.position.y)), 2
-        )  # DEBUG
 
     def rotate(self, dt):
-        self.rotation += PLAYER_TURN_SPEED + dt
-        print(f"Rotation updated to: {self.rotation}")  # DEBUG
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+        print(f"Position updated to: {self.position}")  # DEBUG
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
@@ -35,3 +36,7 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
