f3300ef65bf88c92146348ebcc2cff3f9c1529e7
dustyhardman <40414792+dustyhardman@users.noreply.github.com>
2025-02-25 21:29:17 -0500

    modified:   main.py

--- main.py
+++ main.py
@@ -8,8 +8,11 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
@@ -17,11 +20,12 @@ def main():
                 return
 
         # Update the player
-        player.update(dt)
+        updatable.update(dt)
 
         # Draw Everything
         screen.fill("black")
-        player.draw(screen)
+        for entity in drawable:
+            entity.draw(screen)
         pygame.display.flip()
 
         # Limit framerate to 60 FPS and caculate delta time
