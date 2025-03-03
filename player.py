ce6cc33cf0cab1e839e5a52596338a71c900dfcf
dustyhardman <40414792+dustyhardman@users.noreply.github.com>
2025-03-02 19:27:51 -0500

    modified:   constants.py modified:   player.py

--- player.py
+++ player.py
@@ -6,6 +6,7 @@ from constants import (
     PLAYER_SHOOT_SPEED,
     PLAYER_SPEED,
     PLAYER_TURN_SPEED,
+    PLAYER_SHOOT_COOLDOWN,
 )
 from shot import Shot
 
@@ -14,6 +15,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
         # print(f"Initialized Player Position: {self.position}")  # DEBUG
 
     def draw(self, screen):
@@ -40,10 +42,16 @@ class Player(CircleShape):
             self.move(-dt)
         if keys[pygame.K_SPACE]:
             self.shoot()
+        if self.timer > 0:
+            self.timer -= dt
 
     def shoot(self):
-        shot = Shot(self.position.x, self.position.y)
-        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        if self.timer <= 0:
+            shot = Shot(self.position.x, self.position.y)
+            shot.velocity = (
+                pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+            )
+            self.timer = PLAYER_SHOOT_COOLDOWN
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
