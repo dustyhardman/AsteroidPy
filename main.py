1216f5cfbcc11294a93d2212602c5da31cfa511e
dustyhardman <40414792+dustyhardman@users.noreply.github.com>
2025-04-05 20:00:13 -0400

    modified:   asteroid.py modified:   main.py

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def main():
             for shot in shots:
                 if asteroid.is_colliding_with(shot):
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
 
         # Draw Everything
         screen.fill("black")
